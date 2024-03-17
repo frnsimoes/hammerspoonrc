c1ec5f795623f2cdcdd89a15f4bea433df5565c2
frnsimoes <simoes.grcia@gmail.com>
2024-03-17 17:35:49 -0300

    add init.lua

--- init.lua
+++ init.lua
@@ -1,15 +1,46 @@
-function changeVolume(diff)
-  return function()
-    local current = hs.audiodevice.defaultOutputDevice():volume()
-    local new = math.min(100, math.max(0, math.floor(current + diff)))
-    if new > 0 then
-      hs.audiodevice.defaultOutputDevice():setMuted(false)
-    end
-    hs.alert.closeAll(0.0)
-    hs.alert.show("Volume " .. new .. "%", {}, 0.5)
-    hs.audiodevice.defaultOutputDevice():setVolume(new)
-  end
+-- Define the workspaces and the apps they contain
+local workspace1 = {"iTerm", "Firefox"}
+local workspace2 = {"Google Chrome"}
+local workspace3 = {"Spotify"}
+local workspace6 = {"Telegram"}
+
+
+local function simulateKeystroke(appObject, key)
+    -- delay to ensure the application is fully launched
+    hs.timer.doAfter(1, function()
+        -- focus application window
+        appObject:activate()
+        -- simulate keystroke
+        hs.eventtap.keyStroke({"alt"}, key)
+    end)
 end
 
-hs.hotkey.bind(nil, 'F1', changeVolume(-5))
-hs.hotkey.bind(nil, 'F2', changeVolume(5))
+
+-- Helper function to check if a value exists in a table
+local function isInTable(t, value)
+    for _, v in pairs(t) do
+		print(v)
+        if v == value then
+            return true
+        end
+    end
+    return false
+end
+
+-- Modify the applicationWatcher function
+local function applicationWatcher(appName, eventType, appObject)
+    if (eventType == hs.application.watcher.launched) then
+        if isInTable(workspace1, appName) then
+            simulateKeystroke(appObject, "q")
+	elseif isInTable(workspace2, appName) then
+	    simulateKeystroke(appObject, "w")
+	elseif isInTable(workspace3, appName) then
+	    simulateKeystroke(appObject, "e")
+        elseif isInTable(workspace6, appName) then
+            simulateKeystroke(appObject, "y")
+        end
+    end
+end
+
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
